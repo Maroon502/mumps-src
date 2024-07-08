19978b681bd33b173dc6eae7f5414b5410a231d9
Maroon502 <maroon502@outlook.com>
2024-07-08 22:16:53 +0800

    fix: fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 #![allow(non_snake_case)]
 
 // Ensure the libraries are linked in, despite it not being used directly
-#[cfg(any(feature = "intel-mkl-static", feature = "intel-mlk-system"))]
+#[cfg(any(feature = "intel-mkl-static", feature = "intel-mkl-system"))]
 extern crate intel_mkl_src;
 #[cfg(any(feature = "openblas-static", feature = "openblas-system"))]
 extern crate openblas_src;
