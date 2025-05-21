b24189ce2994a962a9259c7748b9ec973b59c59a
Maroon502 <maroon502@outlook.com>
2025-05-21 18:27:43 +0800

    chore: Bump mumps from 5.7.2 to 5.8.0

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ use cc::Build;
 use coin_build_tools::{coinbuilder,link, utils};
 
 const LIB_NAME: &str = "Mumps";
-const LIB_VERSION: &str = "5.7.2";
+const LIB_VERSION: &str = "5.8.0";
 
 fn main() {
     let download_dir = if let Ok(p) = env::var("MUMPS_DOWNLOAD_DIR") {
@@ -148,7 +148,7 @@ pub fn download_mumps<P: AsRef<Path>>(download_dir: P, lib_version: &str) -> Res
         .as_ref()
         .join(format!("{}_{}", lib_name.to_uppercase(), lib_version));
     if !dest.exists() {
-        let buf = get_agent().get(url.as_str()).call()?.into_reader();
+        let buf = get_agent().get(url.as_str()).call()?.into_body().into_reader();
         let gz_stream = flate2::read::GzDecoder::new(buf);
         let mut ar = tar::Archive::new(gz_stream);
         ar.unpack(&download_dir)?;
@@ -158,11 +158,13 @@ pub fn download_mumps<P: AsRef<Path>>(download_dir: P, lib_version: &str) -> Res
 }
 
 fn get_agent() -> ureq::Agent {
-    ureq::AgentBuilder::new()
-        .tls_connector(std::sync::Arc::new(
-            native_tls::TlsConnector::new().expect("failed to create TLS connector"),
-        ))
+    let tls_config = ureq::tls::TlsConfig::builder()
+                .provider(ureq::tls::TlsProvider::NativeTls)
+                .build();
+    ureq::config::Config::builder()
+        .tls_config(tls_config)
         .build()
+        .new_agent()
 }
 
 #[allow(non_snake_case)]
@@ -178,23 +180,27 @@ fn Mumps_files_common() -> (Vec<String>, Vec<String>) {
         "front_data_mgt_m.F".to_string(),
         "fac_descband_data_m.F".to_string(),
         "fac_future_niv2_mod.F".to_string(),
-        "fac_ibct_data_m.F".to_string(),
         "fac_maprow_data_m.F".to_string(),
         "lr_common.F".to_string(),
+        "lr_stats.F".to_string(),
+        "mumps_comm_buffer_common.F".to_string(),
+        "mumps_intr_types_common.F".to_string(),
         "mumps_l0_omp_m.F".to_string(),
+        "mumps_load.F".to_string(),
         "mumps_memory_mod.F".to_string(),
         "mumps_mpitoomp_m.F".to_string(),
         "mumps_ooc_common.F".to_string(),
-        "mumps_static_mapping.F".to_string(),
         "mumps_pivnul_mod.F".to_string(),
+        "mumps_static_mapping.F".to_string(),
+        "sol_ds_common_m.F".to_string(),
+        "sol_omp_common_m.F".to_string(),
+        "tools_common_m.F".to_string(),
     ];
     let files_common_oth = vec![
-        "sol_ds_common_m.F".to_string(),
-        "mumps_comm_ibcast.F".to_string(),
-        "ana_AMDMF.F".to_string(),
         "ana_blk.F".to_string(),
         "ana_orderings.F".to_string(),
         "ana_set_ordering.F".to_string(),
+        "ana_AMDMF.F".to_string(),
         "bcast_errors.F".to_string(),
         "estim_flops.F".to_string(),
         "mumps_print_defined.F".to_string(),
@@ -206,22 +212,22 @@ fn Mumps_files_common() -> (Vec<String>, Vec<String>) {
         // "mumps_c.c".to_string(),
         "mumps_common.c".to_string(),
         "mumps_config_file_C.c".to_string(),
-        "mumps_io_basic.c".to_string(),
         "mumps_io.c".to_string(),
+        "mumps_io_basic.c".to_string(),
         "mumps_io_err.c".to_string(),
-        "mumps_io_thread.c".to_string(),
-        "mumps_metis64.c".to_string(),
-        "mumps_metis.c".to_string(),
-        "mumps_metis_int.c".to_string(),
         "mumps_numa.c".to_string(),
+        "mumps_io_thread.c".to_string(),
         "mumps_pord.c".to_string(),
-        "mumps_save_restore_C.c".to_string(),
-        "mumps_scotch64.c".to_string(),
+        "mumps_metis.c".to_string(),
+        "mumps_metis64.c".to_string(),
+        "mumps_metis_int.c".to_string(),
         "mumps_scotch.c".to_string(),
+        "mumps_scotch64.c".to_string(),
         "mumps_scotch_int.c".to_string(),
-        "mumps_thread_affinity.c".to_string(),
         "mumps_register_thread.c".to_string(),
         "mumps_thread.c".to_string(),
+        "mumps_thread_affinity.c".to_string(),
+        "mumps_save_restore_C.c".to_string(),
     ];
 
     (files_common_mod, files_common_oth)
@@ -236,44 +242,43 @@ fn Mumps_files_arith(arith: &str) -> (Vec<String>, Vec<String>) {
         "ana_aux_par.F".to_string(),
         "lr_type.F".to_string(),
         "mumps_lr_data_m.F".to_string(),
-        "lr_stats.F".to_string(),
         "lr_core.F".to_string(),
-        "mumps_comm_buffer.F".to_string(),
-        "mumps_load.F".to_string(),
         "ana_lr.F".to_string(),
-        "fac_sol_l0omp_m.F".to_string(),
-        "mumps_ooc_buffer.F".to_string(),
-        "mumps_ooc.F".to_string(),
+        "omp_tps_m.F".to_string(),
         "static_ptr_m.F".to_string(),
         "fac_mem_dynamic.F".to_string(),
-        "omp_tps_m.F".to_string(),
-        "fac_lr.F".to_string(),
-        "fac_front_aux.F".to_string(),
-        "fac_front_type2_aux.F".to_string(),
-        "fac_front_LDLT_type1.F".to_string(),
-        "fac_front_LDLT_type2.F".to_string(),
-        "fac_front_LU_type1.F".to_string(),
-        "fac_front_LU_type2.F".to_string(),
+        "mumps_intr_types.F".to_string(),
         "fac_asm_master_ELT_m.F".to_string(),
         "fac_asm_master_m.F".to_string(),
+        "fac_compact_factors_m.F".to_string(),
+        "mumps_ooc_buffer.F".to_string(),
+        "mumps_ooc.F".to_string(),
+        "fac_front_aux.F".to_string(),
+        "fac_lr.F".to_string(),
+        "fac_front_LU_type1.F".to_string(),
+        "mumps_comm_buffer.F".to_string(),
+        "fac_front_type2_aux.F".to_string(),
+        "fac_front_LU_type2.F".to_string(),
+        "fac_front_LDLT_type1.F".to_string(),
+        "fac_front_LDLT_type2.F".to_string(),
         "fac_sispointers_m.F".to_string(),
         "fac_omp_m.F".to_string(),
         "fac_par_m.F".to_string(),
+        "fac_sol_l0omp_m.F".to_string(),
+        "mumps_config_file.F".to_string(),
         "mumps_mpi3_mod.F".to_string(),
+        "mumps_sol_es.F".to_string(),
         "mumps_save_restore_files.F".to_string(),
         "mumps_save_restore.F".to_string(),
-        "mumps_sol_es.F".to_string(),
         "sol_lr.F".to_string(),
         "sol_omp_m.F".to_string(),
-        "mumps_mpi3_mod.F".to_string(),
     ];
 
     let files_oth = vec![
-        "ana_dist_m.F".to_string(),
         "ana_aux_ELT.F".to_string(),
+        "ana_dist_m.F".to_string(),
         "ana_driver.F".to_string(),
         "ana_LDLT_preprocess.F".to_string(),
-        "ana_mtrans.F".to_string(),
         "ana_reordertree.F".to_string(),
         "arrowheads.F".to_string(),
         "bcast_int.F".to_string(),
@@ -281,10 +286,9 @@ fn Mumps_files_arith(arith: &str) -> (Vec<String>, Vec<String>) {
         "fac_asm_ELT.F".to_string(),
         "fac_asm.F".to_string(),
         "fac_b.F".to_string(),
-        "fac_determinant.F".to_string(),
         "fac_diag.F".to_string(),
-        "fac_dist_arrowheads_omp.F".to_string(),
         "fac_distrib_distentry.F".to_string(),
+        "fac_dist_arrowheads_omp.F".to_string(),
         "fac_distrib_ELT.F".to_string(),
         "fac_driver.F".to_string(),
         "fac_lastrtnelind.F".to_string(),
@@ -294,14 +298,14 @@ fn Mumps_files_arith(arith: &str) -> (Vec<String>, Vec<String>) {
         "fac_mem_stack_aux.F".to_string(),
         "fac_mem_stack.F".to_string(),
         "fac_process_band.F".to_string(),
-        "fac_process_bf.F".to_string(),
         "fac_process_blfac_slave.F".to_string(),
-        "fac_process_blocfacto.F".to_string(),
         "fac_process_blocfacto_LDLT.F".to_string(),
+        "fac_process_blocfacto.F".to_string(),
+        "fac_process_bf.F".to_string(),
+        "fac_process_end_facto_slave.F".to_string(),
         "fac_process_contrib_type1.F".to_string(),
         "fac_process_contrib_type2.F".to_string(),
         "fac_process_contrib_type3.F".to_string(),
-        "fac_process_end_facto_slave.F".to_string(),
         "fac_process_maprow.F".to_string(),
         "fac_process_master2.F".to_string(),
         "fac_process_message.F".to_string(),
@@ -310,16 +314,17 @@ fn Mumps_files_arith(arith: &str) -> (Vec<String>, Vec<String>) {
         "fac_process_rtnelind.F".to_string(),
         "fac_root_parallel.F".to_string(),
         "fac_scalings.F".to_string(),
+        "fac_determinant.F".to_string(),
         "fac_scalings_simScaleAbs.F".to_string(),
         "fac_scalings_simScale_util.F".to_string(),
         "fac_sol_pool.F".to_string(),
         "fac_type3_symmetrize.F".to_string(),
         "ini_defaults.F".to_string(),
         "ini_driver.F".to_string(),
-        "mumps_config_file.F".to_string(),
         "mumps_driver.F".to_string(),
         "mumps_f77.F".to_string(),
         "mumps_iXamax.F".to_string(),
+        "ana_mtrans.F".to_string(),
         "ooc_panel_piv.F".to_string(),
         "rank_revealing.F".to_string(),
         "sol_aux.F".to_string(),
@@ -327,6 +332,7 @@ fn Mumps_files_arith(arith: &str) -> (Vec<String>, Vec<String>) {
         "sol_bwd.F".to_string(),
         "sol_c.F".to_string(),
         "sol_distrhs.F".to_string(),
+        "sol_distsol.F".to_string(),
         "sol_driver.F".to_string(),
         "sol_fwd_aux.F".to_string(),
         "sol_fwd.F".to_string(),
